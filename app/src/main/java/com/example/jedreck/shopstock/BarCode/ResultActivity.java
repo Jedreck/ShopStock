acb927a321e27ebad1186fce0145e48edaa2a257
Jedreck <a302720001@outlook.com>
2018-04-20 00:39:32 +0800

    增加扫描条码的代码，可调用，未实现具体功能

---
+++ app/src/main/java/com/example/jedreck/shopstock/BarCode/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.example.jedreck.shopstock.BarCode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.jedreck.shopstock.R;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
