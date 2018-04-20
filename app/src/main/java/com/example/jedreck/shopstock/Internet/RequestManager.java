12647cafebb47768c9a252a53aaa8b662d779d40
Jedreck <a302720001@outlook.com>
2018-04-20 19:21:51 +0800

    封装获取Request方法，可直接调用获取各自Request

---
+++ app/src/main/java/com/example/jedreck/shopstock/Internet/RequestManager.java
@@ -0,0 +1,55 @@
+package com.example.jedreck.shopstock.Internet;
+
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class RequestManager {
+    private static String URL = "http://rb47h9.natappfree.cc/storage/";
+
+    //获取当日销量前十
+    public static Request getSaleWell() {
+        return new Request.Builder()
+                .url(URL + "SearchSaleWell_Servlet")
+                .build();
+    }
+
+    //获取商品简单信息
+    public static Request getIDLite(RequestBody requestBody) {
+        return new Request.Builder()
+                .url(URL + "SearchIDLite_Servlet")
+                .post(requestBody)
+                .build();
+    }
+
+    //获取商品完整信息
+    public static Request getIDFull(RequestBody requestBody) {
+        return new Request.Builder()
+                .url(URL + "SearchIDFull_Servlet")
+                .post(requestBody)
+                .build();
+    }
+
+    //搜索商品的name或id
+    public static Request getFuzzyIDOrName(RequestBody requestBody) {
+        return new Request.Builder()
+                .url(URL + "SearchFuzzyIDOrName_Servlet")
+                .post(requestBody)
+                .build();
+    }
+
+    //获取紧缺前十的库存商品简单信息
+    public static Request getUnderStock(RequestBody requestBody) {
+        return new Request.Builder()
+                .url(URL + "SearchUnderStock_Servlet")
+                .post(requestBody)
+                .build();
+    }
+
+    //获取入库
+    public static Request getEnterStock(RequestBody requestBody) {
+        return new Request.Builder()
+                .url(URL + "EnterStock_Servlet")
+                .post(requestBody)
+                .build();
+    }
+}
