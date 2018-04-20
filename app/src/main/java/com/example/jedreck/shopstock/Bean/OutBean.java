175f3fdc1cad623b5e359f12e2a3a015ba70bf71
Jedreck <a302720001@outlook.com>
2018-04-20 18:19:55 +0800

    OutBean：增加name属性，增加json2Objectives

--- app/src/main/java/com/example/jedreck/shopstock/Bean/OutBean.java
+++ app/src/main/java/com/example/jedreck/shopstock/Bean/OutBean.java
@@ -1,6 +1,7 @@
 package com.example.jedreck.shopstock.Bean;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
@@ -16,9 +17,17 @@ public class OutBean {
     private String time;
     private String num;
     private String price_out;
+    private String name;
+
 
     public static OutBean objectFromData(String str) {
         return new Gson().fromJson(str, OutBean.class);
+    }
+
+    public static List<OutBean> json2Objectives(String data) {
+        List<OutBean> list = new Gson().fromJson(data, new TypeToken<List<OutBean>>() {
+        }.getType());
+        return list;
     }
 
     public static String objects2Json(List<OutBean> outBeans) {
@@ -55,5 +64,24 @@ public class OutBean {
 
     public void setPrice_out(String price_out) {
         this.price_out = price_out;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return this.getId() + "-" + this.getNum();
+    }
+
+    public static void main(String[] args) {
+        String s = "[{\"id\":\"6907992500133\",\"time\":\"2018-04-20 14:48:24.0\",\"num\":\"10.0\",\"price_out\":\"25.0\"},{\"id\":\"6907992500133\",\"time\":\"2018-04-20 14:48:24.0\",\"num\":\"10.0\",\"price_out\":\"25.0\"}]";
+        List<OutBean> list = OutBean.json2Objectives(s);
+        System.out.println(list.get(0).toString() + list.get(1).toString());
     }
 }
