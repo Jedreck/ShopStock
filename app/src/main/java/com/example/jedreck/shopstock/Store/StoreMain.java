67e5ed505cea47032554e3865e18a6b387ba2802
Jedreck <a302720001@outlook.com>
2018-04-22 20:57:56 +0800

    更改各个活动的启动方式

--- app/src/main/java/com/example/jedreck/shopstock/Store/StoreMain.java
+++ app/src/main/java/com/example/jedreck/shopstock/Store/StoreMain.java
@@ -1,5 +1,6 @@
 package com.example.jedreck.shopstock.Store;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
