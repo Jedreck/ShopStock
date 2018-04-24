32d5288920be7714f12a2697020d77e074f9c396
yiyan666 <1822685810@qq.com>
2018-04-24 09:18:38 +0800

    实现了打电话，调整了布局

--- app/src/main/java/com/example/jedreck/shopstock/Store/CargoAdapter.java
+++ app/src/main/java/com/example/jedreck/shopstock/Store/CargoAdapter.java
@@ -16,16 +16,23 @@ import com.example.jedreck.shopstock.R;
 
 import java.util.List;
 
+import static android.support.v4.content.ContextCompat.startActivity;
+
 /**
  * Created by zjx on 2018/4/20.
  */
 
 public class CargoAdapter extends ArrayAdapter<Cargo> {
     private int resourceId;
-
+    private List<Cargo> tests;
+    private Context context;
+    LayoutInflater layoutInflater;
     public CargoAdapter(Context context, int textViewResourceId, List<Cargo> objects) {
         super(context, textViewResourceId, objects);
         resourceId = textViewResourceId;
+        this.tests=tests;
+        this.context=context;
+        layoutInflater=LayoutInflater.from(context);
     }
 
     @Override
@@ -40,16 +47,20 @@ public class CargoAdapter extends ArrayAdapter<Cargo> {
             viewHolder.cargoInfo = (TextView) view.findViewById(R.id.cargo_info);
             viewHolder.cargoButton = (ImageButton) view.findViewById(R.id.ImageButton);
             view.setTag(viewHolder);
+            viewHolder.cargoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:13821846482"));
+                    context.startActivity(intent);
+                }
+            });
         } else {
             view = converView;
             viewHolder=(ViewHolder)view.getTag();
         }
-//        cargoImage = (ImageView) view.findViewById(R.id.cargo_image);
-//        cargoInfo = (TextView) view.findViewById(R.id.cargo_info);
-//        cargoButton = (ImageButton) view.findViewById(R.id.ImageButton);
         viewHolder.cargoImage.setImageResource(cargo.getImageid());
         viewHolder.cargoInfo.setText(cargo.getInfo());
-        viewHolder.cargoButton.setImageResource(R.drawable.call);
+        viewHolder.cargoButton.setImageResource(R.drawable.call2);
         return view;
     }
     class ViewHolder
